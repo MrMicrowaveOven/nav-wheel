75472719a5be2b9c7c616099b1c1652a62c50be1
mrmicrowaveoven <benj@zagorski.com>
2021-02-21 17:46:32 -0800

    Added mobile alert for non-mobile

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Wheel from './Wheel.js'
 
 function App() {
+  const isMobile = window.innerWidth <= 768;
   return (
-    <div className="App">
-      <Wheel
-        numSpokes={4}
-      />
-    </div>
+    isMobile
+      ? <div className="App">
+          <Wheel
+            numSpokes={4}
+          />
+        </div>
+      : <div className="non-mobile-alert">Sorry, this page is only meant to be viewed on mobile!  Please come back again on a mobile device.</div>
   );
 }
 
