68946dff62f63298e828ddf5bd82002c01bd06b7
mrmicrowaveoven <benj@zagorski.com>
2021-02-21 17:38:55 -0800

    Dragging functionality to keep wheel active is now working

--- src/Wheel.js
+++ src/Wheel.js
@@ -7,22 +7,32 @@ const Wheel = (props) => {
   const [active, setActive] = useState(false)
 
   const fourSpokes = () => {
-    return  <div className="wheel-container">
-              <div className="top">back</div>
-              <div className="row">
-                <div className="left">prev</div>
-                <div className="right">next</div>
-              </div>
-              <div className="bottom">more</div>
-            </div>
+    return  active
+      ? <div
+          className="wheel-container"
+          onTouchEnd={() => setActive(false)}
+          onMouseUp={() => setActive(false)}
+        >
+          <div className="top">back</div>
+          <div className="row">
+            <div className="left">prev</div>
+            <div className="circle"></div>
+            <div className="right">next</div>
+          </div>
+          <div className="bottom">more</div>
+        </div>
+      : <div className="wheel-container">
+          <div
+            className="circle-inactive"
+            onMouseDown={() => setActive(true)}
+            onTouchStart={() => setActive(true)}
+          ></div>
+        </div>
   }
-
-
 
   const spokesOptions = {
     4: fourSpokes()
   }
-
 
   return(
     <div>{spokesOptions[numSpokes]}</div>
