dbcff4479f6c28da10b20d93d6aeec5d3248be90
mrmicrowaveoven <benj@zagorski.com>
2021-02-21 20:11:55 -0800

    Added refs

--- src/Wheel.js
+++ src/Wheel.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './Wheel.css';
 
 const Wheel = (props) => {
@@ -6,20 +6,32 @@ const Wheel = (props) => {
 
   const [active, setActive] = useState(false)
 
+  const showMe = (ref) => {
+    // ReactDOM
+    // .findDOMNode(this.refs[ref])
+    // .getBoundingClientRect(); //outputs <h3> coordinates
+  }
+
+  const top = React.createRef();
+  const left = React.createRef();
+  const right = React.createRef();
+  const bottom = React.createRef();
+
   const fourSpokes = () => {
     return  active
       ? <div
           className="wheel-container active"
           onTouchEnd={() => setActive(false)}
           onMouseUp={() => setActive(false)}
+          onTouchMove={(e) => console.log(e)}
         >
-          <div className="top">back</div>
+          <div className="top" ref={top}>back</div>
           <div className="row">
-            <div className="left">prev</div>
+            <div className="left" ref={left}>prev</div>
             <div className="circle"></div>
-            <div className="right">next</div>
+            <div className="right" ref={right}>next</div>
           </div>
-          <div className="bottom">more</div>
+          <div className="bottom" ref={bottom}>more</div>
         </div>
       : <div className="wheel-container">
           <div
